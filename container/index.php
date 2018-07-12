e0d0f6b14bf0427a523a99d9c51b7a8751a29e50
Trent Harvey <trent@deanshomer.com>
2018-07-12 10:50:44 -0700

    Docker Files

---
+++ container/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE>
+<html>
+<head>
+	<title>DevServer</title>
+</head>
+<body>
+<h1>Welcome to DevServer</h1>
+<p>This is the default webpage of the server. This is meant to be replaced my mounting your local project development directory to /var/app in the Docker Container.</p>
+<p>Keep in mind, the server is expecting to find it's "public" directory in order to server files out of it. Make sure your development directory includes this path.</p>
+</body>
+</html>
